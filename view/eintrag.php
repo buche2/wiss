5c688c78a1fe81f79638a68584dfd717ad321933
Haslebacher Timo <timo.haslebacher@wiss-online.ch>
2018-10-30 13:41:25 +0100

    view für eintrag.php mit Bootstrap vorerst beendet

--- view/eintrag.php
+++ view/eintrag.php
@@ -2,9 +2,12 @@
 <html lang="de">
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>Neuer Eintrag</title>
 	
-	<style type="text/css">
+	<!-- Bootstrap link -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<!-- <style type="text/css">
 		body {
 			background-color: rgb(230, 230, 230);
 		}
@@ -16,24 +19,35 @@
 		}
 
 		#eintrag {
-			position: fixed;
+			position: fixed ;
 		}
-	</style>
+	</style> -->
+
 </head>
 <body>
 	<h1>Schreiben sie einen neuen Eintrag</h1>
-	<div class="form">
-		<form action="" method="" onsubmit="">
+	<div class="form-group">
+		<form action="forum.php" method="post" onsubmit="function insertEintrag();">	
+	<div>
 			<label for="titel">Titel:</label>
-			<input type="text" name="titel" id="titel" required width="">
-			<br><br>
+			<input type="text" class="form-control" name="titel" id="titel" required width="">
+	</div>
+	<div class="form-group">
 			<label for="kategorie">Kategorie:</label>
-			<input type="text" name="kategorie" id="kat"
+			<input type="text" class="form-control" name="kategorie" id="kat"
 			required width="">
-			<br><br>
+	</div>
+	<div class="form-group">
 			<label for="eintrag">Eintrag:</label>
-			<input type="text" name="eintrag" id="eintrag" required width="" height="">
+			<textarea type="text" class="form-control2" name="eintrag" id="eintrag" required rows="10"></textarea>  	
+	</div>
+	<button type="submit" class="btn">Submit</button>
 		</form>
 	</div>
+
+	<!-- Footer (Scripts) -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
