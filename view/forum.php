a6ec05e06d0d45784c2c4da318a95c8d1e7b6f5b
Haslebacher Timo <timo.haslebacher@wiss-online.ch>
2018-11-21 15:52:44 +0100

    toggle function on all

--- view/forum.php
+++ view/forum.php
@@ -7,7 +7,7 @@
     	  <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     	  </div>
         <div class="container border-top border-success">
-          <a href="eintrag.php" id="Antwort">Antwort</a>
+          <a href="eintrag.php">Antwort</a>
           <a href="#" class="float-right show" id="answer1">Antworten anzeigen</a>
         </div>
 		  </div>
@@ -24,14 +24,16 @@
         <div class="container border-bottom border-success">Title  Category  Date  Username</div>
         <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </div>
-        <div class="container border-top border-success"><a href="eintrag.php" id="Antwort">Antwort</a><a href="#" onclick="" class="float-right">Antworten anzeigen</a></div>
+        <div class="container border-top border-success">
+          <a href="eintrag.php">Antwort</a>
+          <a href="#" class="float-right show" id="answer2">Antworten anzeigen</a></div>
       </div>
 
-      <div class="container border border-dark rounded ml-4 mt-2">
+      <div class="container border border-dark rounded ml-4 mt-2 answer2">
         <div class="container border-bottom border-dark">Title  Category  Date  Username</div>
         <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </div>
-        <div class="container border-top border-dark"><a href="eintrag.php" id="Antwort">Antwort</a></div>
+        <div class="container border-top border-dark"><a href="eintrag.php">Antwort</a></div>
       </div>
     </div>
 
@@ -44,6 +46,4 @@
   $(".show").click(function(){
     $("."+$(this).attr('id')).toggle();
   });
-
-
 </script>
