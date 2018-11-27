58f217959a9422d2bf7a96e7f952ea468c360a74
buche2 <buchs.enrico@gmail.com>
2018-11-27 12:04:27 +0100

    shorter regex for phone

--- src/main/java/ch/wiss/regex/PhoneValidator.java
+++ src/main/java/ch/wiss/regex/PhoneValidator.java
@@ -5,8 +5,9 @@ import java.util.regex.Pattern;
 
 public class PhoneValidator {
 
-    public static boolean isValid(String phone){                    // +41 76 674 32949
-        Pattern pattern = Pattern.compile("^(([0-9]{3}[ ]?[0-9]{3})|(\\+[0-9]{2}[ ]?(\\([0-9]\\))?[ ]?[0-9]{2}[ ]?[0-9]{3}))(([ ]?[0-9]){2}){2}$");
+    public static boolean isValid(String phone){                    
+        Pattern pattern = Pattern.compile("^([0-9]{3}|\\+[0-9]{2}[ ]?([ ]?\\([0-9]\\))?[ ]?[0-9]{2})[ ]?[0-9]{3}([ ]?[0-9]{2}){2}$");
+
         Matcher matcher = pattern.matcher(phone);
 
         return matcher.find();
