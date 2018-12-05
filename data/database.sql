9d3863ec12500deacfe2bf38f3a2205e4a2614af
buche2 <buchs.enrico@gmail.com>
2018-12-05 15:44:59 +0100

    insert statements

--- data/database.sql
+++ data/database.sql
@@ -5,8 +5,10 @@ create table User (id int not null auto_increment primary key, gender enum('Herr
 
 create table Forum (id int not null auto_increment primary key, user_id int not null, title varchar(100) not null, category varchar(150), content varchar(5000) not null, FOREIGN KEY(user_id) REFERENCES User(id));
 
-create table Product(id int not null auto_increment primary key, name varchar(40) not null, product_sdesc varchar(100) not null, product_ldesc varchar(1500), price float(0.05) not null, picture varchar(500));
+create table Product(id int not null auto_increment primary key, name varchar(40) not null, product_sdesc varchar(300) not null, product_ldesc text, price float(0.05) not null, picture varchar(500));
 
 create table Orders (id int not null auto_increment primary key, user_id int not null, order_date date not null, d_date date, p_state boolean not null, FOREIGN KEY(user_id) REFERENCES User(id));
 
 create table OrderItem(id int not null auto_increment primary key, orders_id int not null, product_id int not null, product_name varchar(32), product_price int not null, orders_amount int not null, FOREIGN KEY(orders_id) REFERENCES Orders(id), FOREIGN KEY (product_id) REFERENCES Product(id));
+
+INSERT INTO `User` (`id`, `gender`, `firstname`, `lastname`, `username`, `email`, `password`, `birthdate`) VALUES (NULL, 'Herr', 'Hans', 'Bolliger', 'h.bolliger', 'h.bolliger@gmail.com', 'test', '2018-12-04');
