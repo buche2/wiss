7790a6ca651a3cf287a5f5763f6f1b70817db01b
Haslebacher Timo <timo.haslebacher@wiss-online.ch>
2018-12-05 13:55:54 +0100

    new forum model class

--- view/forum.php
+++ view/forum.php
@@ -1,4 +1,4 @@
-<button class="btn btn-outline-success float-right mt-5">Neuer Eintrag</button>
+<button class="btn btn-outline-success float-right mt-5"><a href="index.php?page=forum&action=add" style="color:green;text-decoration:none;">Neuer Eintrag</a></button>
 <h1 class="my-4">Wiss Forum</h1>
 
     <div class="container">
@@ -7,7 +7,7 @@
     	  <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     	  </div>
         <div class="container border-top border-success">
-          <a href="eintrag.php">Antwort</a>
+          <a href="index.php?page=forum&action=add" style="text-decoration:none;">Antwort</a>
           <a href="#" class="float-right show" id="answer1">Antworten anzeigen</a>
         </div>
 		  </div>
@@ -16,7 +16,7 @@
         <div class="container border-bottom border-dark">Title  Category  Date  Username</div>
         <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </div>
-        <div class="container border-top border-dark"><a href="eintrag.php">Antwort</a>
+        <div class="container border-top border-dark"><a href="index.php?page=forum&action=add" style="text-decoration:none;">Antwort</a>
         </div>
       </div>
 
@@ -25,7 +25,7 @@
         <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </div>
         <div class="container border-top border-success">
-          <a href="eintrag.php">Antwort</a>
+          <a href=index.php?page=forum&action=add" style="text-decoration:none;">Antwort</a>
           <a href="#" class="float-right show" id="answer2">Antworten anzeigen</a></div>
       </div>
 
@@ -33,7 +33,7 @@
         <div class="container border-bottom border-dark">Title  Category  Date  Username</div>
         <div class="container py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </div>
-        <div class="container border-top border-dark"><a href="eintrag.php">Antwort</a></div>
+        <div class="container border-top border-dark"><a href=index.php?page=forum&action=add" style="text-decoration:none;">Antwort</a></div>
       </div>
     </div>
 
