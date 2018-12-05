6c47fabfa0955aa4164d046141a585ae2a7cd439
Faisst Erik <erik.faisst@wiss-online.ch>
2018-12-05 15:58:16 +0100

    Einrücken

--- view/detail.php
+++ view/detail.php
@@ -1,28 +1,28 @@
-
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <?php
        echo <<<EOF
     <div class="IMG">
-      <img src="{$product->picture}" class="float-left" alt="" width="150" height="130">
+      <img src="{$this->$product->picture}" class="float-left" alt="" width="150" height="130">
     </div>
-    <div class="Preis">
-      <dl>
-        <dt>{$product->price}</dt>
-      </dl>
-    </div>
+      <div class="Preis">
+        <dl>
+          <dt>{$this->$product->price}</dt>
+        </dl>
+      </div>
     <div class="Name">
-      <h3>{$product->name}</h3>
-      <p> {$product->product_sdesc}</p>
+      <h3>{$this->$product->name}</h3>
+      <p> {$this->$product->product_sdesc}</p>
     </div>
     <div class="Beschreibung">
-      <p>{$product->product_ldesc}</p>
+      <p>{$this->$product->product_ldesc}</p>
     </div>
-  </div>
-</div>
-<button type="button" class="btn btn-outline-success" name="Warenkorb" onclick="myFunction()">Warenkorb</button>
+
+    <button type="button" class="btn btn-outline-success" name="Warenkorb" onclick="myFunction()">Warenkorb</button>
 EOF;
  ?>
+  </div>
+</div>
     <script>
       function myFunction() {
         alert("Success! Ihr Atikel ist jetzt im Warenkorb");
