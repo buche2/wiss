212c819f528977ff94e7f89399620d34733edce4
walder christof <student@NBBESC810.wiss-sc.local>
2018-12-05 15:35:49 +0100

    fix database OrdersItem

--- data/database.sql
+++ data/database.sql
@@ -5,8 +5,8 @@ create table User (id int not null auto_increment primary key, gender enum('Herr
 
 create table Forum (id int not null auto_increment primary key, user_id int not null, title varchar(100) not null, category varchar(150), content varchar(5000) not null, FOREIGN KEY(user_id) REFERENCES User(id));
 
-create table Product(id int not null auto_increment primary key, name varchar(40) not null, product_sdesc varchar(100) not null, product_ldesc varchar(1500), price float(0.05) not null, picture varchar(2000));
+create table Product(id int not null auto_increment primary key, name varchar(40) not null, product_sdesc varchar(100) not null, product_ldesc varchar(1500), price float(0.05) not null, picture varchar(500));
 
-create table Order (id int not null auto_increment primary key, user_id int not null, order_date date not null, d_date date, p_state boolean not null, FOREIGN KEY(user_id) REFERENCES User(id));
+create table Orders (id int not null auto_increment primary key, user_id int not null, order_date date not null, d_date date, p_state boolean not null, FOREIGN KEY(user_id) REFERENCES User(id));
 
-create table OrderItem(id int not null auto_increment primary key, Order_id int not null, product_id int not null, product_name varchar, product_price int not null, o_amount int not null, REFERENCES Order(id), REFERENCES Product(id), REFERENCES Product(name), REFERENCES Product(price));
+create table OrderItem(id int not null auto_increment primary key, orders_id int not null, product_id int not null, product_name varchar(32), product_price int not null, orders_amount int not null, FOREIGN KEY(orders_id) REFERENCES Orders(id), FOREIGN KEY (product_id) REFERENCES Product(id));
