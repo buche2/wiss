530487b951c37fd23f99af4a3e07cf66d2d1c88a
walder christof <student@NBBESC810.wiss-sc.local>
2018-12-12 15:37:54 +0100

    warenkorb design

--- view/warenkorb.php
+++ view/warenkorb.php
@@ -1,30 +1,20 @@
 <form class="warenkorb" action="index.html" method="post">
-  <div class="main-cart">
-    <div class="container">
-      <h2>Warenkorb</h2>
-      <ul cl ass="list-group">
-        <li class="list-group-item"><img style=" height: 70px; weight: 70px; "src="https://yt3.ggpht.com/a-/AN66SAwbvKCY2WMuccEwVggeo5onjONkU7ITPiVC=s900-mo-c-c0xffffffff-rj-k-no" alt="">
-          Der geliebte Homr Simson wurde ihrem Warenkorb Hinzugefügt.
-          <div class="text-right">
-            <button  type="button" class="btn btn-default btn-xs">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item"><img style=" height: 70px; weight: 70px; "src="https://image.dhgate.com/0x0s/f2-albu-g4-M00-5E-38-rBVaEFne1SaAKnLZAAKJz2ymJcQ359.jpg/f-r-kawasaki-zx6r-zx-6r-636-2007-2008-motorrad.jpg" alt="">
-          Hier können alle möglichen details Stehen!
-          <div class="text-right">
-            <button  type="button" class="btn btn-default btn-xs">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item"><img style=" height: 70px; weight: 70px; "src="https://i.pinimg.com/originals/f2/94/27/f29427b012484c73543a5faeff8674cb.jpg" alt="">
-          Hier steht die Produktbeschreibung, und dahinter der Preis.
-          <div class="text-right">
-            <button  type="button" class="btn btn-default btn-xs">Delete</button>
-          </div>
-        </li>
-      </ul>
-      <ul>
-
-      </ul>
-    </div>
-  </div>
+  <table  class="table  table-hover">
+    <thead>
+      <tr id="main-table" style="">
+        <th>Menge</th>
+        <th>Artikel</th>
+        <th>Einzel Preis</th>
+        <th>GesamtPreis</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Menge</td>
+          <td><img src="Bildpfad" >Artikelname</td>
+          <td>Einzelpreis kosten</td>
+          <td>Einzelpreis Kosten * Menge = Gesamtkosten</td>
+        </tr>
+      </tbody>
+  </table>
 </form>
