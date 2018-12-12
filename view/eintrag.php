e4f15790f3681499eb38393f3939da13eef6f5cb
Haslebacher Timo <timo.haslebacher@wiss-online.ch>
2018-12-12 14:45:55 +0100

    dynamic Date and functioning Forumentry

--- view/eintrag.php
+++ view/eintrag.php
@@ -2,29 +2,16 @@
 	<h1>Schreiben sie einen neuen Eintrag</h1>
 	<div class="form-group">
 			<label for="titel">Titel:</label>
-			<input type="text" class="form-control" name="titel" id="titel" required>
+			<input type="text" class="form-control" name="title" id="titel" required>
 	</div>
 	<div class="form-group">
 			<label for="kategorie">Kategorie:</label>
-			<input type="text" class="form-control" name="kategorie" id="kat"	required>
+			<input type="text" class="form-control" name="category" id="kat"	required>
 	</div>
 	<div class="form-group">
 			<label for="eintrag">Eintrag:</label>
-			<textarea type="text" class="form-control" name="eintrag" id="eintrag" required rows="10"></textarea>
+			<textarea type="text" class="form-control" name="content" id="eintrag" required rows="10"></textarea>
 	</div>
 	<button onclick="function getDate();" type="submit" class="btn">Submit</button>
 </form>
 <br>
-
-<script>
-   var time = new Date();
-
-   function getDate() {
-   var year = time.getYear()+1900;
-   var month = time.getMonth()+1;
-   var day = time.getDate();
-   var date = year + "-" + month + "-" + day;
-
-   return date;
-   };
- </script>
