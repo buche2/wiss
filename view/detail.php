2e5af7f5e59755bedd15ca6c1ac5d0fc5541fc09
Faisst Erik <erik.faisst@wiss-online.ch>
2018-12-19 14:40:22 +0100

    Image not availabl

--- view/detail.php
+++ view/detail.php
@@ -1,35 +1,37 @@
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <?php
-       echo <<<EOF
-        <div class="IMG">
-          <img src="assets/img/Products/{$this->product->id}.jpg" onerror="myError()" class="rounded float-left" alt="" hspace="20" width="400" height="400">
-        </div>
-        <div class="Name">
-          <h3>{$this->product->name}</h3>
-        <div class="Preis">
-            <dl>
-              <dt>{$this->product->price}.- CHF</dt>
-            </dl>
-        </div>
-          <p> {$this->product->product_sdesc}</p>
-        </div>
-        </br>
-        </br>
-        <div class="Beschreibung">
-          <p>{$this->product->product_ldesc}</p>
-        </div>
-        <button type="button" class="btn btn-outline-success" name="Warenkorb" onclick="myFunction()">Warenkorb</button>
+    $filename = 'assets/img/Products/'.$this->product->id.'.jpg';
+    if (file_exists($filename)) {
+      $filename;
+    } else {
+      $filename = 'assets/img/imagenotavailable.jpg';
+    }
+    echo <<<EOF
+      <div class="IMG">
+        <img src="$filename" class="rounded float-left" alt="" hspace="20" width="400" height="400">        </div>
+      <div class="Name">
+        <h3>{$this->product->name}</h3>
+      <div class="Preis">
+        <dl>
+          <dt>{$this->product->price}.- CHF</dt>
+        </dl>
+      </div>
+        <p> {$this->product->product_sdesc}</p>
+      </div>
+      </br>
+      </br>
+      <div class="Beschreibung">
+        <p>{$this->product->product_ldesc}</p>
+      </div>
+      <button type="button" class="btn btn-outline-success" name="Warenkorb" onclick="myFunction()">Warenkorb</button>
 EOF;
-?>
+    ?>
   </div>
 </div>
 
 <script>
   function myFunction() {
     alert("Success! Ihr Atikel ist jetzt im Warenkorb");
-  }
-  function myError() {
-    this.src="assets/img/imagenotavailable.jpg";
   }
 </script>
