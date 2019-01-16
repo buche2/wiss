d9b4c880d58a231c96004699d8b3023b11f27694
buche2 <buchs.enrico@gmail.com>
2019-01-16 15:39:39 +0100

    send mail to input mail via template

--- controller/KontaktController.php
+++ controller/KontaktController.php
@@ -5,6 +5,9 @@ namespace wiss\controller;
 use wiss\controller\AbstractController;
 use wiss\database\PDOConnection;
 use wiss\phpmailer\PHPMailer;
+use wiss\model\UserModel;
+use wiss\helper\Session;
+use wiss\helper\Request;
 
 class KontaktController extends AbstractController{
 
@@ -16,50 +19,62 @@ class KontaktController extends AbstractController{
 	    parent::display($content);
 	}
 
-	function send(){
+	public function send(){
 		if (isset($_POST["submit"])) {
+			$vorname = $_POST["form_name"];
+			$name = $_POST["form_lastname"];
+			$email = $_POST["form_email"];
+			$Betreff = $_POST["form_betreff"];
+			$Mitteilung = $_POST["form_message"];
 
-			$mail = new PHPMailer(false);                          // Passing `true` enables exceptions
+			$this->user = new UserModel();
+			$this->user->name = $name;
+			$message = parent::loadView('email');
 
-		    $an = "wissmodul133@gmail.com";
-		    $Vorname = $_POST["form_name"];
-		    $name = $_POST["form_lastname"];
-		    $email = $_POST["form_email"];
-		    $Betreff = $_POST["form_betreff"];
-		    $Mitteilung = $_POST["form_message"];
-		    $message = "Vorname: " . $_POST['form_name']. "<br>" . "Name:" . $_POST['form_lastname']. "<br>" . "Email: " . $_POST['form_email']. "<br>" .$_POST['form_betreff']. "<br>" .$_POST['form_message'];
+			$this->email($this->user, Request::getPost('form_betreff'), $message);
 
-		    //Server settings
-		    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-		    $mail->isSMTP();                                      // Set mailer to use SMTP
-		    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-		    $mail->Username = 'wissmodul133@gmail.com';           // SMTP username
-		    $mail->Password = 'abcd.1234';                        // SMTP password
-		    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		    $mail->Port = 587;                                    // TCP port to connect to
-
-		    //Recipients
-		    $mail->setFrom( $email, $Vorname, $name);
-				$mail->addAddress($email);
-		    $mail->addAddress('wissmodul133@gmail.com');          // Add a recipient
-		    //$mail->addAddress('ellen@example.com');             // Name is optional
-		    //$mail->addReplyTo('info@example.com', 'Information');
-		    //$mail->addCC('cc@example.com');
-		    //$mail->addBCC('bcc@example.com');
-
-		    //Content
-		    $mail->isHTML(true);                                  // Set email format to HTML
-		    $mail->Subject = $_POST["form_betreff"];
-		    $mail->Body    = $message;
-		    $mail->AltBody = $_POST["form_email"];
-
-
-		    $mail->send();
-		    $this->title = "Kontakt send";
+		    //
+		  $this->title = "Kontakt send";
 			$content = parent::loadView('send');
+
 			parent::display($content);
 		}
+	}
+
+	private function email($user, $subject, $message){
+			$mail = new PHPMailer(false);                          // Passing `true` enables exceptions
+
+			$an = "wissmodul133@gmail.com";
+
+
+
+			//Server settings
+			$mail->SMTPDebug = 0;                                 // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = 'wissmodul133@gmail.com';           // SMTP username
+			$mail->Password = 'abcd.1234';                        // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+
+
+			//Recipients
+			$mail->setFrom( $an, 'WISS', 'Forum');
+			$mail->addAddress($user->email);
+			$mail->addBCC('wissmodul133@gmail.com');          // Add a recipient
+			//$mail->addAddress('ellen@example.com');             // Name is optional
+			//$mail->addReplyTo('info@example.com', 'Information');
+			//$mail->addCC('cc@example.com');
+			//$mail->addBCC('bcc@example.com');
+
+			//Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = $subject;
+			$mail->Body    = $message;
+			$mail->AltBody = $user->email;
+
+			$mail->send();
 	}
 
 }
