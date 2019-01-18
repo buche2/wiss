325d50b3bbe174222f25eccb824af0dadd8e1829
buche2 <buchs.enrico@gmail.com>
2019-01-18 13:34:17 +0100

    new project for m326 - exam down and uploader

---
+++ exam-service/src/main/java/ch/wiss/exam/ExamServiceApplication.java
@@ -0,0 +1,36 @@
+package ch.wiss.exam;
+
+import ch.wiss.exam.model.Highscore;
+import ch.wiss.exam.repositories.HighscoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class ExamServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamServiceApplication.class, args);
+    }
+
+    @Autowired
+    private HighscoreRepository highscoreRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Highscore h = new Highscore("Diego");
+        //highscoreRepository.save(h);
+
+        List<Highscore> highscoreList = highscoreRepository.findAllByNameLike("%Di%");
+        
+
+        for(Highscore h : highscoreList){
+            System.out.println(h.getName());
+        }
+
+    }
+}
+
---
+++ exam-service/src/main/java/ch/wiss/exam/repositories/HighscoreRepository.java
@@ -0,0 +1,13 @@
+package ch.wiss.exam.repositories;
+
+import ch.wiss.exam.model.Highscore;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface HighscoreRepository extends JpaRepository<Highscore, Integer>, CrudRepository<Highscore,Integer>, JpaSpecificationExecutor {
+
+    public List<Highscore> findAllByNameLike(String name);
+}
