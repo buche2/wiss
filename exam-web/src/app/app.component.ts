0dc4bc7c7cff293c2414293a892a2cf6a2d1d480
Kilian <kilualthaus@hotmail.com>
2019-01-26 15:25:31 +0100

    übersetzung

--- exam-web/src/app/app.component.ts
+++ exam-web/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'race';
+  title = 'Exam Web App';
 
   constructor(translate: TranslateService
   ) {
