b720cc76728849f2369ded946999d2497f37dda0
Kilian <kilualthaus@hotmail.com>
2019-01-26 13:40:52 +0100

    route role für admin hinzugefügt

--- exam-web/src/app/admin/admin.module.ts
+++ exam-web/src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { RoleComponent } from './role/role.component';
-import {AdminRoutingModule} from './admin-routing.module';
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [UserComponent, RoleComponent],
--- exam-web/src/app/app-routing.module.ts
+++ exam-web/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'}
+  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
 ];
 
 @NgModule({
